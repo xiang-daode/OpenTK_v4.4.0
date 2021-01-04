05cab609b4421f7600809fb5a1b2658eaba04d1b
xiang-daode <76726932+xiang-daode@users.noreply.github.com>
2021-01-04 21:48:36 +0800

    VS2019+OpenTK4.4.0
    Three dimensional simulation of robot,by xiang-daode

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using OpenTK.Mathematics;
+using OpenTK.Windowing.Desktop;
+
+namespace LearnOpenTK
+{
+    public static class Program
+    {
+        private static void Main()
+        {
+            var nativeWindowSettings = new NativeWindowSettings()
+            {
+                Size = new Vector2i(1920, 1080),
+                Title = "OpenTK4.4.0 @No6-MultipleLights",
+            };
+
+            using (var window = new Window(GameWindowSettings.Default, nativeWindowSettings))
+            {
+                window.Run();
+            }
+        }
+    }
+}
